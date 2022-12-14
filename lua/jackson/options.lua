647ba0fbd1b5aebeeb80933ae335ecd30d811f39
JcksonDev <65576130+JcksonDev@users.noreply.github.com>
2022-12-14 04:16:04 -0800

    added options

---
+++ lua/jackson/options.lua
@@ -0,0 +1,41 @@
+local options = {
+    backup = false,
+    clipboard = "unnamedplus",
+    cmdheight = 2,
+    completeopt = { "menuone", "noselect" },
+    conceallevel = 0,
+    fileencoding = "utf-8",
+    hlsearch = true,
+    ignorecase = true,
+    mouse = "a",
+    pumheight = 10,
+    showmode = true,
+    smartcase = true,
+    smartindent = true,
+    splitbelow = true,
+    splitright = true,
+    swapfile = false,
+    timeoutlen = 1000,
+    undofile = true,
+    updatetime = 300,
+    expandtab = true,
+    shiftwidth = 4,
+    tabstop = 4,
+    cursorline = true,
+    number = true,
+    relativenumber = true,
+    numberwidth = 4,
+    signcolumn = "yes",
+    wrap = false,
+    scrolloff = 8,
+    sidescrolloff = 0,
+    guifont = "monospace:h17",
+}
+
+vim.opt.shortmess:append "c"
+
+vim.cmd "set whichwrap+=<,>[,]h,l"
+
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
